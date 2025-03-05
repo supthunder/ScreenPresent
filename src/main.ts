27a57adbeb8f34b44bf81c0eaf3c8a55d094b586
supthunder <ejohn3@umbc.edu>
2025-03-05 00:32:04 -0500

    FLOATING BUTTONS LETS GOOOOOO

--- src/main.ts
+++ src/main.ts
@@ -7,23 +7,36 @@ let controlWindow: BrowserWindow | null = null;
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 500,
-    height: 400,
+    height: 200,
     resizable: false,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
     },
-    titleBarStyle: 'hiddenInset',
-    backgroundColor: '#ffffff'
+    frame: false,
+    transparent: true,
+    backgroundColor: '#00000000',
+    hasShadow: false
   });
 
   mainWindow.loadFile(path.join(__dirname, '../src/index.html'));
+  
+  // Position the window in the center of the screen
+  const { screen } = require('electron');
+  const primaryDisplay = screen.getPrimaryDisplay();
+  const { width: screenWidth, height: screenHeight } = primaryDisplay.workAreaSize;
+  const windowWidth = 500;
+  const windowHeight = 200;
+  mainWindow.setPosition(
+    Math.floor(screenWidth / 2 - windowWidth / 2),
+    Math.floor(screenHeight / 2 - windowHeight / 2)
+  );
 }
 
 function createControlWindow() {
   controlWindow = new BrowserWindow({
-    width: 120,
-    height: 40,
+    width: 160,
+    height: 50,
     frame: false,
     resizable: false,
     alwaysOnTop: true,
@@ -32,11 +45,21 @@ function createControlWindow() {
       contextIsolation: false
     },
     transparent: true,
-    backgroundColor: '#00ffffff'
+    backgroundColor: '#00000000',
+    hasShadow: false,
+    skipTaskbar: true,
+    focusable: false
   });
 
   controlWindow.loadFile(path.join(__dirname, '../src/control.html'));
   controlWindow.setVisibleOnAllWorkspaces(true);
+  
+  // Position the window in the top-center of the screen
+  const { screen } = require('electron');
+  const primaryDisplay = screen.getPrimaryDisplay();
+  const { width: screenWidth } = primaryDisplay.workAreaSize;
+  const windowWidth = 160;
+  controlWindow.setPosition(Math.floor(screenWidth / 2 - windowWidth / 2), 20);
 }
 
 app.whenReady().then(() => {
